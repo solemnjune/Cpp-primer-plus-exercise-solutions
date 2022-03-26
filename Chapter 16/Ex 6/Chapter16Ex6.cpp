845950835499d707dbe414122eadfa7d9d791256
solemnjune <55949679+solemnjune@users.noreply.github.com>
2022-03-26 06:10:13 +0300

    Create Chapter16Ex6.cpp

---
+++ Chapter 16/Ex 6/Chapter16Ex6.cpp
@@ -0,0 +1,98 @@
+//компилировать вместе с Customer.cpp
+#include "pch.h"
+#include <iostream>
+#include <queue>
+#include <cstdlib> // для rand() и srand() 
+#include <ctime> // для time() 
+#include "Customer.h"
+
+
+const int MIN_PER_HR = 60;
+//-------------------------------------------------------------------------------------------------
+bool newcustomer(double x); // появился ли новый клиент? 
+//-------------------------------------------------------------------------------------------------
+int main()
+{
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	using std::ios_base;
+	// setting things up
+	std::srand(std::time(0));    // случайная инициализация rand() 
+
+	// Подготовка 
+	cout << "Case Study: Bank of Heather Automatic Teller\n";
+	cout << "Enter maximum size of queue: ";		// ввод максимального размера очереди 
+	int qs;
+	cin >> qs;
+	std::queue <Item> line;
+
+	cout << "Enter the number of simulation hours: ";	// ввод количества эмулируемых часов 
+	int hours;					 // часы эмуляции 
+	cin >> hours;
+	// Эмуляция будет запускать один цикл в минуту 
+	long cyclelimit = MIN_PER_HR * hours; // количество циклов 
+
+	// Ввод количества клиентов в час 
+	cout << "Enter the average number of customers per hour: ";
+	double perhour;         // среднее количество появлений за час 
+	cin >> perhour;
+	double min_per_cust;   // среднее время между появлениями 
+	min_per_cust = MIN_PER_HR / perhour;
+
+	Item temp;              // данные нового клиента 
+	long turnaways = 0;     // не допущены в полную очередь 
+	long customers = 0;     // присоединены к очереди 
+	long served = 0;        // обслужены во время эмуляции 
+	long sum_line = 0;      // общая длина очереди 
+	int wait_time = 0;      // время до освобождения банкомата 
+	long line_wait = 0;     // общее время в очереди 
+
+	// Запуск моделирования 
+	for (int cycle = 0; cycle < cyclelimit; cycle++)
+	{
+		if (newcustomer(min_per_cust)) // есть подошедший клиент 
+		{
+			if (line.size()==qs)
+				turnaways++;
+			else
+			{
+				customers++;
+				temp.set(cycle);    // cycle = время прибытия 
+				line.push(temp); // добавление новичка в очередь 
+			}
+		}
+		if (wait_time <= 0 && !line.empty())
+		{
+
+			temp=line.front();       // обслуживание следующего клиента 
+			line.pop();
+			wait_time = temp.ptime(); // в течение wait_time минут 
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time > 0)
+			wait_time--;
+		sum_line += line.size();
+	}
+
+	// Вывод результатов 
+	if (customers > 0)
+	{
+		cout << "customers accepted: " << customers << endl;	// принято клиентов 
+		cout << "  customers served: " << served << endl;		// обслужено клиентов 
+		cout << "         turnaways: " << turnaways << endl;	// не принято клиентов 
+		cout << "average queue size: ";							// средний размер очереди 
+		cout.precision(2);
+		cout.setf(ios_base::fixed, ios_base::floatfield);
+		cout << (double)sum_line / cyclelimit << endl;
+		cout << " average wait time: "							// среднее время ожидания (минут) 
+			<< (double)line_wait / served << " minutes\n";
+	}
+	else
+		cout << "No customers!\n";								// клиентов нет 
+	cout << "Done!\n";
+
+	
+	return 0;
+}
