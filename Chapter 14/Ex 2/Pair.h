08fa92d77cf7948420fb6c0610309c86ed96c77b
solemnjune <55949679+solemnjune@users.noreply.github.com>
2022-03-26 04:09:53 +0300

    Create Pair.h

---
+++ Chapter 14/Ex 2/Pair.h
@@ -0,0 +1,16 @@
+#pragma once
+template <class T1, class T2>
+class Pair
+{
+	T1 a;
+	T2 b;
+public:
+	Pair(const T1 & aval, const T2 & bval) : a(aval), b(bval) { }
+	Pair() {};
+
+	T1 & first() { return a; }
+	T2 & second() { return b; }
+	T1 first() const { return a; }
+	T2 second() const { return b; }
+	//Pair& operator=();
+};
